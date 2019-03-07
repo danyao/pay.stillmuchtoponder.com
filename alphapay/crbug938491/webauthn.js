240e78fafe49c6a491373c0a31f0c177a533cbef
Danyao Wang <danyao.wang@gmail.com>
2019-03-07 17:57:30 -0500

    Clean up crbug938491 example

--- alphapay/crbug938491/webauthn.js
+++ alphapay/crbug938491/webauthn.js
@@ -3,10 +3,7 @@
  */
 
 const ERROR='error';
-const INFO='status';
-
-// Simulated server-side data.
-let _server = {};
+const INFO='info';
 
 /**
  * Helper function to append a log message to screen.
@@ -15,6 +12,7 @@ function log(type, message) {
   let output = document.querySelector('#webauthn-logs');
   if (!output) {
     document.body.insertAdjacentHTML('beforeend', '<div id="webauthn-logs"></div>');
+    output = document.querySelector('#webauthn-logs');
   }
   output.insertAdjacentHTML('beforeend', `<pre class="${type}">${message}</pre>`);
 }
@@ -37,9 +35,10 @@ function registerCredential() {
     return;
   }
 
+  console.log('registerCredential');
+
   let challenge = randomChallenge(32);
-  _server.challenge = challenge.join();
-  log(INFO, `Client challenge: ${_server.challenge}`);
+  log(INFO, `Client challenge: ${challenge.join()}`);
 
   let publicKey = {
     challenge,
@@ -67,59 +66,8 @@ function registerCredential() {
   };
 
   navigator.credentials.create({ publicKey }).then(credential => {
-    simulateServerSideValidateCredential(credential);
     console.log(credential);
   }).catch(error => {
     log(ERROR, `Failed to create credential: ${error}`);
-  });
-}
-
-/**
- * Simulates credential validation steps that would normally be done on
- * the server side.
- * Returns the validated ID that would be stored on server side, or
- * null if validation fails.
- */
-function simulateServerSideValidateCredential(credential) {
-  // Verify that credeitnail contains the same data this client presented
-  // to generate the credential.
-  const utf8Decoder = new TextDecoder('utf-8');
-  const decodedClientData = utf8Decoder.decode(
-    credential.response.clientDataJSON);
-  const clientDataObj = JSON.parse(decodedClientData);
-
-  if (clientDataObj.type != 'webauthn.create') {
-    return null;
-  }
-  // TODO: add other checks
-
-  log(INFO, `Received credential.id: ${credential.id}`);
-  log(INFO, `Received clientData: ${JSON.stringify(clientDataObj, undefined, 2)}`);
-}
-
-/**
- * Authenticates the user using the public key ID from #webauthn-cred.
- */
-function authenticate() {
-  if (!navigator || !navigator.credentials) {
-    showError('WebAuthn not supported.');
-    return;
-  }
-
-  const credentialId = document.querySelector('#webauthn-cred').value;
-  const publicKey = {
-    challenge: randomChallenge(32),
-    allowCredentials: [{
-      id: Uint8Array.from(credentialId, c => c.charCodeAt(0)),
-      type: 'public-key',
-      transports: ['usb', 'ble', 'nfc'],
-    }],
-    timeout: 60000
-  };
-
-  navigator.credentials.get({publicKey}).then(assertion => {
-    console.log(assertion);
-  }).catch(error => {
-    console.log(error);
   });
 }
